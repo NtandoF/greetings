274065ea5d6016ebe96f5cd77940e47d7c4104cf
Ntando <ntandofitshaneversace@gmail.com>
2018-06-05 12:08:34 +0200

    changes made

--- tests/greet.test.js
+++ tests/greet.test.js
@@ -11,4 +11,26 @@ describe('Greetings languages', function() {
     var greeter = GreetingFactory()
     assert.equal(greeter.greeting('BlessMess','French'), 'Bonjour, BlessMess');
   });
+  it('Greetings should return undefined when the name is undefined', function() {
+    var greeter = GreetingFactory();
+   assert.equal(greeter.greeting("English", ''), undefined);
+});
+  });
+
+  describe('Greetings Counter', function() {
+    it('should return the correct number of people greeted even if greeted more then once', function() {
+      var greeter = GreetingFactory()
+      greeter.greeting('Ntando', 'English')
+      greeter.greeting('Ntando', 'English')
+      greeter.greeting('Ntando', 'English')
+      assert.equal(greeter.counter(), 1);
+    });
+
+      it('should return the correct number of different people greeted', function() {
+        var greeter = GreetingFactory()
+        greeter.greeting('Yawa', 'English')
+        greeter.greeting('Ntando', 'English')
+        greeter.greeting('Ziyanda', 'English')
+        assert.equal(greeter.counter(), 3);
+      });
 });
